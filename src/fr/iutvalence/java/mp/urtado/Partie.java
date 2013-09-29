d4b6b2c3266f767f79f52ed8e90aadf27489a071
sebastienjean <baz.jean@gmail.com>
2013-09-29 11:49:41 +0200

    Reviewed code, added some TODOs
    Removed useless (because unused) classes.

--- src/fr/iutvalence/java/mp/urtado/Partie.java
+++ src/fr/iutvalence/java/mp/urtado/Partie.java
@@ -1,5 +1,8 @@
 package fr.iutvalence.java.mp.urtado;
 
+// TODO (fix) rename class (english)
+// TODO (think about it) comply with instructions : (rule 1) write something 
+// if and only if you make immediate usage of it.
 public class Partie
 {
 
